19d9cb592d85dc561ea61f74dc784bfd8b041f3d
Calabonga <calabonga@outlook.com>
2017-02-07 10:15:55 +0700

    web.csproj created

--- generators/Web/index.js
+++ generators/Web/index.js
@@ -2,12 +2,14 @@
 var Generator = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
+var uuid = require('uuid');
 var mkdirp = require('mkdirp');
 
 module.exports = Generator.extend({
 
   initializing: function () {
     this.props = {};
+    this.props.projectGuid = uuid.v4();
   },
 
   prompting: function () { },
@@ -18,12 +20,19 @@ module.exports = Generator.extend({
 
   writing: function () {
 
-    mkdirp(this.options.appName +"/" +this.options.appName + '.Web');
+    var webPath = this.options.appName + "/" + this.options.appName + ".Web";
+    mkdirp(webPath);
 
-    // this.fs.copy(
-    //   this.templatePath('dummyfile.txt'),
-    //   this.destinationPath('dummyfile.txt')
-    // );
+    this.fs.copy(
+      this.templatePath('staticRoot/*.*'),
+      this.destinationPath(webPath),
+      { globOptions: { dot: true } }
+    );
+    this.fs.copyTpl(
+      this.templatePath(this.options.appName + ".Web.csproj"),
+      this.destinationPath(webPath + "/" + this.options.appName + ".Web.csproj"),
+      { globOptions: { dot: true }, projectGuid: this.props.projectGuid.toUpperCase() }
+    );
   },
   conflicts: function () { },
 
