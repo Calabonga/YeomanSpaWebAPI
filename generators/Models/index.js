83f2c062e8f304d1748aa6fc8fd03a23dfc6a386
Calabonga <calabonga@outlook.com>
2017-02-07 08:57:07 +0700

    folder already workd fine

---
+++ generators/Models/index.js
@@ -0,0 +1,35 @@
+'use strict';
+var Generator = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+var mkdirp = require('mkdirp');
+
+module.exports = Generator.extend({
+
+  initializing: function () {
+    this.props = {};
+  },
+
+  prompting: function () { },
+
+  configuring: function () { },
+
+  default: function () { },
+
+  writing: function () {
+
+    mkdirp(this.options.appName +"/" +this.options.appName + '.Models');
+
+    // this.fs.copy(
+    //   this.templatePath('dummyfile.txt'),
+    //   this.destinationPath('dummyfile.txt')
+    // );
+  },
+  conflicts: function () { },
+
+  install: function () { },
+
+  end: function () {
+    this.log(yosay('Model was created...'));
+  }
+});
